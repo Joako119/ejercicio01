43154a0357cc4b3f6759b15e08684987dd54b57d
Joako <Joako@DESKTOP-3MNMSK9>
2023-05-11 11:08:47 -0300

    Agregar archivos de proyecto.

---
+++ ejercicio.01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio._01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1;
+            int numero2;
+            int suma;
+            Console.Write("ingresar el primer numero");
+            numero1 = int.Parse( Console.ReadLine());
+            Console.Write("ingresar el segundo numero");
+            numero2 = int.Parse( Console.ReadLine());
+            suma = numero1 + numero2;
+
+            Console.Write($"la suma es { suma}");
+
+        }
+    }
+}
